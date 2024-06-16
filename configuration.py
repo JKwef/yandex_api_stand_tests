ba50c74f7e1447a5ecd3c815b2400ef5c2996cc3
JKwef <vanek2002002@gmail.com>
2024-06-16 23:57:43 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://be43cecf-c841-465c-a53d-650325ef846e.serverhub.praktikum-services.ru/"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
